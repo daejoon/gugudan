f0f36aabf0aebec34e3d5f53278d2c9a7f77f877
daejoon.ko <kkode1911@gmail.com>
2018-03-01 08:23:13 +0900

    설정 추가

--- src/main/kotlin/d2/Application.kt
+++ src/main/kotlin/d2/Application.kt
@@ -1,6 +1,43 @@
 package d2
 
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.Banner
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.info.BuildProperties
+import org.springframework.core.env.Environment
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
 /**
  * @Author kdj
  * @Date 2018. 3. 1.
  */
+@RestController
+@SpringBootApplication
+class Application: CommandLineRunner {
+    private val log = LoggerFactory.getLogger(Application::class.java)
+
+    @Autowired
+    lateinit var env: Environment
+
+    @Autowired(required = false)
+    var buildInfo: BuildProperties? = null
+
+    override fun run(vararg args: String?) {
+        log.info("Profile: '{}'", env.activeProfiles)
+        log.info("Version: '{}'", buildInfo?.version ?: "Failed to load.")
+    }
+
+    @GetMapping(value = arrayOf("", "/"))
+    fun hello() = "GuGuDan"
+}
+
+fun main(args: Array<String>) {
+    SpringApplicationBuilder(Application::class.java)
+            .sources(Application::class.java)
+            .bannerMode(Banner.Mode.LOG)
+            .run(*args)
+}
